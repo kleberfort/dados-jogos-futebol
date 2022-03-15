f226eb8ede108af684d2548bc8fbb99e7db9407b
kleberfort <kleberfort@gmail.com>
2022-03-14 21:35:01 -0300

    Adicionado o código para abas

--- app/src/main/java/fort/kleber/dadosjogosdefutebol/MainActivity.java
+++ app/src/main/java/fort/kleber/dadosjogosdefutebol/MainActivity.java
@@ -3,12 +3,38 @@ package fort.kleber.dadosjogosdefutebol;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+import fort.kleber.dadosjogosdefutebol.fragment.HomeFragment;
+import fort.kleber.dadosjogosdefutebol.fragment.InglesFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        smartTabLayout = findViewById(R.id.viewPagerTab);
+        viewPager = findViewById(R.id.viewPager);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                .add("Home", HomeFragment.class)
+                        .add("Inglês", InglesFragment.class)
+                        .create()
+        );
+
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
+
     }
 }
---
+++ app/src/main/java/fort/kleber/dadosjogosdefutebol/fragment/InglesFragment.java
@@ -0,0 +1,29 @@
+package fort.kleber.dadosjogosdefutebol.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fort.kleber.dadosjogosdefutebol.R;
+
+
+public class InglesFragment extends Fragment {
+
+
+
+    public InglesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ingles, container, false);
+    }
+}
